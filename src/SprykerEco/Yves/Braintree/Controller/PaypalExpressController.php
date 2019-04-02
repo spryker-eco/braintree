be9f92f5fe9c5cf75f8c4bdcefb0890a942fcfd4
vhrychenko <aptah199494@gmail.com>
2019-04-02 12:45:42 +0300

    Controller with success/cancel/failure actions

---
+++ src/SprykerEco/Yves/Braintree/Controller/PaypalExpressController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SprykerEco\Yves\Braintree\Controller;
+
+use Spryker\Yves\Kernel\Controller\AbstractController;
+
+class PaypalExpressController extends AbstractController
+{
+    public function successAction()
+    {
+
+    }
+
+    public function failureAction()
+    {
+
+    }
+
+    public function cancelAction()
+    {
+
+    }
+}
