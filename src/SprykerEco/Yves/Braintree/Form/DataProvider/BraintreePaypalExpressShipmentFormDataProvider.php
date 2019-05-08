ca3fae3bd4026dad4281f389a81143f6060535c6
vhrychenko <aptah199494@gmail.com>
2019-05-08 11:34:52 +0300

    Fix unneeded plugin suffix in Provider class

--- src/SprykerEco/Yves/Braintree/Form/DataProvider/BraintreePaypalExpressShipmentFormDataProvider.php
+++ src/SprykerEco/Yves/Braintree/Form/DataProvider/BraintreePaypalExpressShipmentFormDataProvider.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace SprykerEco\Yves\Braintree\Plugin;
+namespace SprykerEco\Yves\Braintree\Form\DataProvider;
 
 use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
 use Spryker\Yves\Kernel\AbstractPlugin;
@@ -14,7 +14,7 @@ use Spryker\Yves\StepEngine\Dependency\Form\StepEngineFormDataProviderInterface;
 /**
  * @method \SprykerEco\Yves\Braintree\BraintreeFactory getFactory()
  */
-class BraintreePaypalExpressShipmentFormDataProviderPlugin extends AbstractPlugin implements StepEngineFormDataProviderInterface
+class BraintreePaypalExpressShipmentFormDataProvider extends AbstractPlugin implements StepEngineFormDataProviderInterface
 {
     /**
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
