908a36c34e887b5a6f03fff4bcb34a4d6f6b754b
vhrychenko <aptah199494@gmail.com>
2019-05-08 10:00:27 +0300

    Fix

--- src/SprykerEco/Service/Braintree/Model/TokenGenerator/TokenGenerator.php
+++ src/SprykerEco/Service/Braintree/Model/TokenGenerator/TokenGenerator.php
@@ -42,7 +42,9 @@ class TokenGenerator implements TokenGeneratorInterface
 
         $this->configure();
 
-        return ClientToken::generate();
+        static::$clientToken = ClientToken::generate();
+
+        return static::$clientToken;
     }
 
     /**
