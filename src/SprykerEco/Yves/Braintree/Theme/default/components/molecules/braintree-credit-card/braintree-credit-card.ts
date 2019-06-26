a2927465eb3715cc89783368d8d59bcf03fa8107
nikita-lysenko <nikita.lysenko@spryker.com>
2019-06-26 18:43:03 +0300

    ECO-3042:Braintree/Adyen Credit card modules overwrite each other

---
+++ src/SprykerEco/Yves/Braintree/Theme/default/components/molecules/braintree-credit-card/braintree-credit-card.ts
@@ -0,0 +1,57 @@
+import PaymentForm from '../payment-form/payment-form';
+
+export default class BraintreeCreditCard extends PaymentForm {
+    form: HTMLFormElement;
+    braintreeCreditCardMethod: HTMLElement;
+
+    readonly paymentMethodName: string = 'braintreeCreditCard';
+    readonly paymentMethodTypeName: string = 'CreditCard';
+
+    protected readyCallback(): void {
+        this.form = <HTMLFormElement>document.getElementById(`${this.formId}`);
+        this.braintreeCreditCardMethod = <HTMLElement>this.form.querySelector(`.${this.jsName}__method`);
+
+        super.readyCallback();
+    }
+
+    protected loadBraintree(): void {
+        super.loadBraintree();
+
+        if (this.braintreeCreditCardMethod) {
+            this.braintreeSetupSettings.id = this.formId;
+            this.braintreeSetupSettings.hostedFields = {
+                styles: {
+                    'input': {
+                        'font-size': '14px',
+                        'color': '#333',
+                        'font-family': 'Arial, sans-serif'
+                    },
+                    '::-webkit-input-placeholder': {
+                        'color': '#bbb'
+                    },
+                    ':-moz-placeholder': {
+                        'color': '#bbb'
+                    },
+                    '::-moz-placeholder': {
+                        'color': '#bbb'
+                    },
+                    ':-ms-input-placeholder': {
+                        'color': '#bbb'
+                    }
+                },
+                number: {
+                    selector: `.${this.jsName}__number`,
+                    placeholder: '4111 1111 1111 1111'
+                },
+                cvv: {
+                    selector: `.${this.jsName}__cvv`,
+                    placeholder: '123'
+                },
+                expirationDate: {
+                    selector: `.${this.jsName}__expiration-date`,
+                    placeholder: 'MM/YYYY'
+                }
+            };
+        }
+    }
+}
--- src/SprykerEco/Yves/Braintree/Theme/default/components/molecules/credit-card/index.ts
+++
@@ -1,4 +0,0 @@
-import './credit-card.scss';
-import register from 'ShopUi/app/registry';
-export default register('credit-card', () => import(/* webpackMode: "lazy" */'./credit-card'));
-
