d0b5d7c2c5dbacbe78a6199ff78b4167652761e9
Dan Miniyarov <dan.miniyarov@spryker.com>
2022-04-20 18:19:24 +0300

    SUPESC-33: Fixed an incorrect deprecation message

--- src/SprykerEco/Zed/Braintree/Business/BraintreeFacade.php
+++ src/SprykerEco/Zed/Braintree/Business/BraintreeFacade.php
@@ -250,7 +250,7 @@ class BraintreeFacade extends AbstractFacade implements BraintreeFacadeInterface
      *
      * @api
      *
-     * @deprecated Use {@link \SprykerEco\Zed\Braintree\Business\BraintreeFacade::checkoutPostSaveHook()}
+     * @deprecated Use {@link \SprykerEco\Zed\Braintree\Business\BraintreeFacade::executeCheckoutPostSaveHook()}
      *
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
      * @param \Generated\Shared\Transfer\CheckoutResponseTransfer $checkoutResponse
